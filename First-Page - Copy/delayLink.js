39181aef54bfb35f7e32872d04c40da4d0de0a18
lilipod <138423672+lisedo123@users.noreply.github.com>
2023-11-01 19:09:07 -0500

    Delete First-Page - Copy/delayLink.js

--- First-Page - Copy/delayLink.js
+++
@@ -1,9 +0,0 @@
-$("a.link[href]").click(function(e){
-    e.preventDefault();
-    if (this.href) {
-        var target = this.href;
-        setTimeout(function(){
-            window.location = target;
-        }, 8000);
-    }
-});
